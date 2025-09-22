262b8569a4287b5124939d3e8fba4f285eec021e
Avinash <avithe1@gmail.com>
2025-09-22 16:00:39 +0530

    responseive cartitem

--- components/CartItem.tsx
+++ components/CartItem.tsx
@@ -8,18 +8,18 @@ const CartItem = ({ product }: { product: Product }) => {
   return (
     <div className="p-2 border rounded border-gray-400 flex justify-between items-center">
       <div className="flex gap-2 items-center">
-        <div className="relative h-[100px] w-[100px] object-">
+        <div className="relative md:h-[100px] md:w-[100px]  h-[50px] w-[50px] object-">
           <Image src={product.image} sizes="100%" fill alt={product.title} />
         </div>
         <div>
-          <h2 className="text-xl font-bold truncate flex-shrink-0">
+          <h2 className="text-sm md:text-xl md:font-bold truncate flex-shrink-0 text-wrap ">
             {product.title}
           </h2>
         </div>
       </div>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 border-l border-gray-700 px-3">
         <div>Price : {product.price} per item</div>
-        <div className="self-end"><UpdateProduct product={product} /></div>
+        <div className="self-center"><UpdateProduct product={product} /></div>
       </div>
     </div>
   );
--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -4,7 +4,7 @@ import ProductCard from "./ProductCard";
 
 const ProductGrid = ({ products }: { products: Product[] }) => {
   return (
-    <div className="grid grid-cols-3 w-full gap-3 p-5">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full gap-3 p-5">
       {products.length
         ? products.map((product) => (
             <ProductCard key={product.id} product={product} />
