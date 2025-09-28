76f057ca22be629482d393c39181431436e0a252
Avinash <avithe1@gmail.com>
2025-09-28 09:42:35 +0530

    search input transitions

--- components/NavSearch.tsx
+++ components/NavSearch.tsx
@@ -27,20 +27,48 @@ const NavSearch = () => {
     dispatch(clearSearchTerm());
   };
 
-  return !isOpen ? (
-    <Search onClick={() => setIsOpen(true)} />
-  ) : (
+  return (
     <div className="flex items-center gap-2">
       <input
-        className="border border-gray-700 rounded w-[200px] px-2 py-0.2"
-        autoFocus
+        id="myInput"
+        className={` transition-all duration-150 border border-gray-700 rounded-full  ${
+          !isOpen ? "w-[0px] border-0" : " w-[200px] px-4 py-0.2 "
+        }`}
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
-      <Search onClick={handleSearch} />
-      <X onClick={clear} />
+      <Search
+        className="cursor-pointer"
+        onClick={
+          isOpen
+            ? handleSearch
+            : () => {
+                setIsOpen(true);
+                document.getElementById("myInput")?.focus();
+              }
+        }
+      />
+      <X
+        onClick={clear}
+        className={`${isOpen ? "block" : "hidden"} cursor-pointer`}
+      />
     </div>
   );
+
+  // return !isOpen ? (
+  //   <Search onClick={() => setIsOpen(true)} />
+  // ) : (
+  //   <div className="flex items-center gap-2">
+  //     <input
+  //       className="border border-gray-700 rounded w-[200px] px-2 py-0.2"
+  //       autoFocus
+  //       value={search}
+  //       onChange={(e) => setSearch(e.target.value)}
+  //     />
+  //     <Search onClick={handleSearch} />
+  //     <X onClick={clear} />
+  //   </div>
+  // );
 };
 
 export default NavSearch;
