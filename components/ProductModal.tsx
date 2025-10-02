df70f7fc0245bce5695dfc840c0347ecafa57507
Avinash <avithe1@gmail.com>
2025-10-02 18:16:15 +0530

    modal accessibility

--- components/ProductModal.tsx
+++ components/ProductModal.tsx
@@ -3,6 +3,38 @@ import { createPortal } from "react-dom";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
+const ModalContent = ({
+  handleBackdropClick,
+  children,
+}: {
+  handleBackdropClick: () => void;
+  children: React.ReactNode;
+}) => {
+  useEffect(() => {
+    const mc = document.getElementById("modalcontent");
+    if (mc) {
+      console.log("focus on mc");
+      mc.focus();
+      mc.tabIndex = -1;
+    }
+  }, []);
+
+  return (
+    <div
+      className="fixed inset-0 z-50 bg-black/40 flex items-center justify-center w-full backdrop-blur-sm"
+      onClick={handleBackdropClick}
+    >
+      <div
+        className="w-[80%] max-w-[700px] h-[70dvh] bg-background border-2 rounded-2xl flex flex-col py-6"
+        onClick={(e) => e.stopPropagation()}
+        id="modalcontent"
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+
 export function Modal({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   const [mounted, setMounted] = useState(false);
@@ -34,19 +66,11 @@ export function Modal({ children }: { children: React.ReactNode }) {
     router.back();
   };
 
-  const modalContent = (
-    <div
-      className="fixed inset-0 z-50 bg-black/40 flex items-center justify-center w-full backdrop-blur-sm"
-      onClick={handleBackdropClick}
-    >
-      <div
-        className="w-[80%] max-w-[700px] h-[70dvh] bg-background border-2 rounded-2xl flex flex-col py-6"
-        onClick={(e) => e.stopPropagation()}
-      >
-        {children}
-      </div>
-    </div>
+  return createPortal(
+    <ModalContent
+      handleBackdropClick={handleBackdropClick}
+      children={children}
+    />,
+    document.body
   );
-
-  return createPortal(modalContent, document.body);
 }
