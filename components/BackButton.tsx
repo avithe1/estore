320f01040ecd2d9e587d76456831aa05ab44077a
Avinash <avithe1@gmail.com>
2025-10-02 18:04:17 +0530

    accessbile back button

--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -6,12 +6,12 @@ import { ChevronLeft } from "lucide-react";
 const BackButton = () => {
   const router = useRouter();
   return (
-    <div className="flex items-center mt-4 cursor-pointer ml-4" onClick={() => router.back()}>
+    <button className="flex items-center mt-4 cursor-pointer ml-3 px-1" onClick={() => router.back()}>
       <div>
         <ChevronLeft />
       </div>{" "}
       <div>Back</div>
-    </div>
+    </button>
   );
 };
 
