65167d1da3483dc640729df7d4791ca3d544fe0a
Avinash <avithe1@gmail.com>
2025-10-17 20:18:18 +0530

    loading skeleton on filteration

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -5,15 +5,18 @@ import ProductCard from "./ProductCard";
 import { useSearchParams, useRouter } from "next/navigation";
 import { createQueryString } from "@/lib/utils";
 import { Search, X } from "lucide-react";
+import ProductsCardSkeleton from "./ProductCardsSkeleton";
 
 const ProductGrid = ({ products }: { products: Product[] }) => {
   const searchParams = useSearchParams();
   const router = useRouter();
   const searchTerm = searchParams.get("search") || "";
   const sortBy = searchParams.get("sort") || "";
+  const [isFiltering, setIsFiltering] = useState<boolean>(true);
   const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
 
   useEffect(() => {
+    setIsFiltering(true);
     let fp = [];
     if (searchTerm.length) {
       fp = [
@@ -37,7 +40,7 @@ const ProductGrid = ({ products }: { products: Product[] }) => {
           break;
       }
     }
-
+    setIsFiltering(false);
     setFilteredProducts(fp);
   }, [searchTerm, sortBy]);
 
@@ -48,13 +51,16 @@ const ProductGrid = ({ products }: { products: Product[] }) => {
           {searchTerm.length ? (
             <>
               <div className="font-bold flex items-center gap-2">
-                <div className="flex items-center"><Search size={14}/>:</div><div className="text-blue-500">{searchTerm}</div>
+                <div className="flex items-center">
+                  <Search size={14} />:
+                </div>
+                <div className="text-blue-500">{searchTerm}</div>
               </div>
               <button
                 className="px-3 py-0.5 rounded-2xl bg-gray-600 hover:bg-gray-500 cursor-pointer"
                 onClick={() => router.replace(createQueryString("", sortBy))}
               >
-                <X size={14}/>
+                <X size={14} />
               </button>
             </>
           ) : null}
@@ -88,21 +94,25 @@ const ProductGrid = ({ products }: { products: Product[] }) => {
           ) : null}
         </div>
       </div>
-      <div
-        className={
-          filteredProducts.length
-            ? "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full gap-3"
-            : "w-full flex flex-1 justify-center items-center"
-        }
-      >
-        {filteredProducts.length ? (
-          filteredProducts.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))
-        ) : (
-          <div>No products available</div>
-        )}
-      </div>
+      {isFiltering ? (
+        <ProductsCardSkeleton />
+      ) : (
+        <div
+          className={
+            filteredProducts.length
+              ? "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full gap-3"
+              : "w-full flex flex-1 justify-center items-center"
+          }
+        >
+          {filteredProducts.length ? (
+            filteredProducts.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))
+          ) : (
+            <div>No products available</div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
